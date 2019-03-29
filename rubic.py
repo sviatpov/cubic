7df0269a0268ef6a392be7c81d95d228a7246595
sviatpov <sviat.povod@gmail.com>
2019-03-29 19:59:13 +0200

    rubic

---
+++ rubic.py
@@ -0,0 +1,102 @@
+import numpy as np
+
+# faces = ['front', 'back', 'right', 'left', 'upper', 'down']
+# numpy = [  0,        1,      2,       3  ,   4  ,       5]
+
+class rubic():
+    def __init__(self, size=3):
+
+        self.cub = np.zeros((6,size,size), dtype='uint8')
+        self.size = size
+        for i, c in enumerate(self.cub):
+            c += i
+
+        self.slice = np.array([i for i in range(self.size)])
+        self.zero = np.zeros(self.size, dtype='uint8')
+        self.last = np.ones(self.size, dtype='uint8') * -1
+        # print(self.cub)
+
+        self.fb = [self._face_([2,5,3,4]),
+                   np.concatenate((self.slice, self.zero, self.slice, self.last)),
+                   np.concatenate((self.zero, self.slice, self.last, self.slice))]
+        self.fa = [self._face_([4,2,5,3]),
+                   np.concatenate((self.last, self.slice, self.zero, self.slice)),
+                   np.concatenate((self.slice, self.zero, self.slice, self.last))]
+
+        self.rb = [self._face_([1,5,0,4]),
+                   np.concatenate((self.slice, self.slice, self.slice, self.slice)),
+                   np.concatenate((self.zero, self.last, self.last, self.last))]
+        self.ra = [self._face_([4,1,5,0]),
+                   np.concatenate((self.slice, self.slice, self.slice, self.slice)),
+                   np.concatenate((self.last, self.zero, self.last, self.last))]
+
+        self.lb = [self._face_([0,5,1,4]),
+                   np.concatenate((self.slice, self.slice, self.slice, self.slice)),
+                   np.concatenate((self.zero, self.zero, self.last, self.zero))]
+        self.la = [self._face_([4,0,5,1]),
+                   np.concatenate((self.slice, self.slice, self.slice, self.slice)),
+                   np.concatenate((self.zero, self.zero, self.zero, self.last))]
+
+        self.bb = [self._face_([3,5,2,4]),
+                    np.concatenate((self.slice, self.last, self.slice, self.zero)),
+                    np.concatenate((self.zero, self.slice, self.last, self.slice))]
+        self.ba = [self._face_([4,3,5,2]),
+                   np.concatenate((self.zero, self.slice, self.last, self.slice )),
+                   np.concatenate((self.slice,self.zero, self.slice, self.last))]
+
+        self.ub = [self._face_([2,0,3,1]),
+                   np.concatenate((self.zero, self.zero, self.zero, self.zero)),
+                   np.concatenate((self.slice, self.slice, self.slice, self.slice))]
+        self.ua = [self._face_([1, 2, 0, 3]),
+                   np.concatenate((self.zero, self.zero, self.zero, self.zero)),
+                   np.concatenate((self.slice, self.slice, self.slice, self.slice))]
+
+        self.db = [self._face_([2,1,3,0]),
+                   np.concatenate((self.last, self.last, self.last, self.last)),
+                   np.concatenate((self.slice, self.slice, self.slice, self.slice))]
+        self.da = [self._face_([0, 2,1,3]),
+                   np.concatenate((self.last, self.last, self.last, self.last)),
+                   np.concatenate((self.slice, self.slice, self.slice, self.slice))]
+
+    def _face_(self, fc):
+        bef = np.concatenate((np.ones(self.size, dtype='uint8')*fc[0],
+                              np.ones(self.size, dtype='uint8')*fc[1],
+                              np.ones(self.size, dtype='uint8')*fc[2],
+                              np.ones(self.size, dtype='uint8')*fc[3]))
+        return bef
+
+
+    def _rot90(self,front,b,a, is_clock=True):
+        if is_clock:
+            self.cub[front] = np.rot90(self.cub[front], k=3)
+            self.cub[tuple(b)] = self.cub[tuple(a)]
+        else:
+            self.cub[front] = np.rot90(self.cub[front], k=1)
+            self.cub[tuple(a)] = self.cub[tuple(b)]
+
+
+    def F(self):
+        self._rot90(0, self.fb, self.fa)
+    def R(self):
+        self._rot90(2, self.rb, self.ra)
+    def Rs(self):
+        self._rot90(2, self.rb, self.ra, False)
+    def Fs(self):
+        self._rot90(0, self.fb, self.fa, False)
+    def L(self):
+        self._rot90(3, self.lb, self.la)
+    def Ls(self):
+        self._rot90(3, self.lb, self.la, False)
+
+if __name__ == "__main__":
+    r = rubic()
+    r.Ls()
+    print(r.cub)
+    # x = np.array([[0, 1, 2],[3, 4, 5],[6, 7, 8],[9, 10, 11]])
+    # rows = np.array([[0, 0],[3, 3]], dtype=np.intp)
+    # columns = np.array([[0, 2],[0, 2]], dtype=np.intp)
+    # print(x)
+    # print(x[rows, columns])
+    # rows = np.array([0, 3], dtype=np.intp)
+    # columns = np.array([0, 2], dtype=np.intp)
+    # print(x[rows[:, np.newaxis], columns])
